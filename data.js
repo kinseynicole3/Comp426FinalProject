0d2ed2111933d2582c64347826f26c0b26a4a75d
Kinsey Ness <nessk@live.unc.edu>
2019-11-17 15:41:43 -0500

    data file added. home page styled

---
+++ data.js
@@ -0,0 +1,29 @@
+const parkingDecks = [{
+    id: 1,
+    name: "Cobb Deck",
+    address: "Paul Green Dr, Chapel Hill, NC 27514", 
+    description: "Park free after 5 on weeknights and weekends",
+}, {
+    id: 2,
+    name: "Bowles Lot",
+    address: "101 N Columbia St, Chapel Hill, NC 27514",
+    description: "South Campus",
+},
+{
+    id: 3,
+    name: "Manning Lot",
+    address: "238 Manning Dr, Chapel Hill, NC 27599",
+    description: "Some info",
+},
+{
+    id: 4,
+    name: "Rams Head Parking Deck",
+    address: "330 Ridge Rd Rams Head Parking Deck, Chapel Hill, NC 27514",
+    description: "Park free after 5 on weeknights and weekends",
+},
+{
+    id: 5,
+    name: "Wallace Parking Deck",
+    address: "150 E Rosemary St, Chapel Hill, NC 27514",
+    description: "Off campus",
+}];
--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
+export const $root = $('#root');
 
+
+$(function() {
+    loadParking();
+});
+
+export const renderListItem = function(deck) {
+    return `
+    <div id="${deck.id}">
+        <section class="section" style="border-bottom-style: solid; border-width: 0.5px">
+        <div class="container">
+            <div class="content has-text-left">
+                <div class="title" style="color: white"><a>${deck.name}</a></div>
+                <div class="subtitle" style="color: white">${deck.address}</div>
+                <div class="subtitle" style="color:white">Notes: ${deck.description}</div>
+            </div>
+            </div>
+        </section>
+    </div>
+    `
+}
+
+
+export const loadParking = function() {
+    parkingDecks.forEach(deck => {
+        $root.append(renderListItem(deck));
+    })
+}
