65697232f70345260ca9ac0790f6a4ae07285538
Kinsey Ness <nessk@live.unc.edu>
2019-11-19 16:50:22 -0500

    map api added

--- data.js
+++ data.js
@@ -3,27 +3,37 @@ const parkingDecks = [{
     name: "Cobb Deck",
     address: "Paul Green Dr, Chapel Hill, NC 27514", 
     description: "Park free after 5 on weeknights and weekends",
+    xCoordinate: 35.911919,
+    yCoordinate: -79.04556
 }, {
     id: 2,
     name: "Bowles Lot",
     address: "101 N Columbia St, Chapel Hill, NC 27514",
     description: "South Campus",
+    xCoordinate: 35.901696,
+    yCoordinate: -79.045181
 },
 {
     id: 3,
     name: "Manning Lot",
     address: "238 Manning Dr, Chapel Hill, NC 27599",
     description: "Some info",
+    xCoordinate: 35.900322,
+    yCoordinate: -79.039598
 },
 {
     id: 4,
     name: "Rams Head Parking Deck",
     address: "330 Ridge Rd Rams Head Parking Deck, Chapel Hill, NC 27514",
     description: "Park free after 5 on weeknights and weekends",
+    xCoordinate: 35.906187,
+    yCoordinate: -79.045563
 },
 {
     id: 5,
     name: "Wallace Parking Deck",
     address: "150 E Rosemary St, Chapel Hill, NC 27514",
     description: "Off campus",
+    xCoordinate: 35.914924,
+    yCoordinate: -79.054215
 }];
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 export const $root = $('#root');
+export const $section = $('#section1');
+
 
 
 $(function() {
@@ -11,7 +13,7 @@ export const renderListItem = function(deck) {
         <section class="section" style="border-bottom-style: solid; border-width: 0.5px">
         <div class="container">
             <div class="content has-text-left">
-                <div class="title" style="color: white"><a>${deck.name}</a></div>
+                <div class="title deckname" id="${deck.id}" style="color: white">${deck.name}</div>
                 <div class="subtitle" style="color: white">${deck.address}</div>
                 <div class="subtitle" style="color:white">Notes: ${deck.description}</div>
             </div>
@@ -21,9 +23,71 @@ export const renderListItem = function(deck) {
     `
 }
 
+export const loadMap = function() {
+    console.log(event.target.id);
+    let deck = parkingDecks.find(function(element) {
+        console.log(element.id);
+        return element.id == event.target.id;
+    });
+
+    $root.append(renderMap(deck.xCoordinate, deck.yCoordinate));
+}
+
+export const renderMap = function(x, y) {
+    return `
+    <div id='map' style='width: 400px; height: 300px;'></div>
+    <script>
+        mapboxgl.accessToken =
+            'pk.eyJ1IjoiY29tcDQyNmZpbmFsIiwiYSI6ImNrMzZidGNsZDAwMzEzbXJ4bXFnM3loYjgifQ.gyiwVu9eUjqg-If1v-gK0A';
+            var map = new mapboxgl.Map({
+                container: 'map', 
+                style: 'mapbox://styles/mapbox/streets-v11',
+                center: [${y}, ${x}],
+                zoom: 16 
+                });
+                map.addControl(new mapboxgl.NavigationControl());
+                map.on('load', function() {
+                    map.loadImage('./location1.png', function(error, image) {
+                        if (error) throw error;
+                        map.addImage('cat', image);
+                        map.addLayer({
+                            "id": "points",
+                            "type": "symbol",
+                            "source": {
+                            "type": "geojson",
+                            "data": {
+                            "type": "FeatureCollection",
+                            "features": [{
+                            "type": "Feature",
+                            "geometry": {
+                                "type": "Point",
+                                "coordinates": [${y},${x}]
+                            }
+                        }]
+                        }
+                    },
+                    "layout": {
+                        "icon-image": "cat",
+                        "icon-size": 0.1
+                    }
+                });
+            });
+        });
+    </script>
+    `
+}
 
 export const loadParking = function() {
+    
     parkingDecks.forEach(deck => {
         $root.append(renderListItem(deck));
-    })
+    });
+    
+    $(document).on("click", ".deckname", function(event) {
+        event.preventDefault();
+        $section.empty();
+        $root.empty();
+        loadMap(event);
+    });
+
 }
