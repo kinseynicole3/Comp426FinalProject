987f64680efca724cf5b97272e3289761bae38f2
btguin <btguin@gmail.com>
2019-11-27 14:05:33 -0500

    updated JS to open and close modals

--- src/index.js
+++ src/index.js
@@ -1,7 +1,53 @@
 export const $root = $('#root');
 export const $section = $('#section1');
 
+var myAccountButton = document.getElementById("myAccount")
+var myAccountModal = document.getElementById("myAccountModal")
+var myAccountClose = document.getElementById("myAccountClose")
 
+myAccountButton.onclick = function() {
+    myAccountModal.style.display = 'block';
+}
+
+myAccountClose.onclick = function() {
+    myAccountModal.style.display = 'none';
+}
+
+var createDeckButton = document.getElementById("createDeck")
+var createDeckModal = document.getElementById("createDeckModal")
+var createDeckClose = document.getElementById("createDeckClose")
+
+createDeckButton.onclick = function() {
+    createDeckModal.style.display = 'block';
+}
+
+createDeckClose.onclick = function() {
+    createDeckModal.style.display = 'none';
+}
+
+var loginButton = document.getElementById("login")
+var loginModal = document.getElementById("loginModal")
+var loginClose = document.getElementById("loginClose")
+
+loginButton.onclick = function() {
+    loginModal.style.display = 'block';
+}
+
+loginClose.onclick = function() {
+    loginModal.style.display = 'none';
+}
+
+var signupButton = document.getElementById("signup")
+var signupModal = document.getElementById("signupModal")
+var signupClose = document.getElementById("signupClose")
+
+signupButton.onclick = function() {
+    signupModal.style.display = 'block';
+}
+
+signupClose.onclick = function() {
+    signupModal.style.display = 'none';
+}
 
 $(function() {
     loadParking();
