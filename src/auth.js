a188659eeeab312ca87567c552110af6cfca7399
btguin <btguin@gmail.com>
2019-11-29 16:21:28 -0500

    added abilitity to upload data from firebase and changed format of decks

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,39 @@
+// get data
+db.collection('decks').get().then(snapshot => {
+    setupDecks(snapshot.docs);
+})
+
+const deckList = document.querySelector('.decks');
+
+const setupDecks = (data) => {
+
+    let html = '';
+    data.forEach(doc => {
+        const theDeck = doc.data();
+        const div = `
+        <div class="card">
+      <div class="card-content">
+        <div>
+            <p class="title is-2">${theDeck.deck}</p>
+            <p class="subtitle is-4">${theDeck.address}</p>
+            <h1>${theDeck.notes}</p>
+        </div>
+      </div>
+      <footer class="card-footer">
+          <div class="buttons">
+              <button class="button is-primary">Save</button>
+              <button class="button is-primary">Edit</button>
+              <button class="button is-primary">Delete</button>
+            </div>
+        </footer>
+    </div>
+        `;
+        html += div;
+    });
+
+    deckList.innerHTML = html;
+}
+
 // listen for auth status changes
 auth.onAuthStateChanged(user => {
     if (user) {
