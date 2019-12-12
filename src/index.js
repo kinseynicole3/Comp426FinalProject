a54f6b4e44397eb559bae54697737178ba121e60
owen8 <owen8@owen8-pc.home>
2019-12-11 22:02:21 -0500

    added debouncing

--- src/index.js
+++ src/index.js
@@ -40,6 +40,9 @@ loginClose.onclick = function() {
 var signupButton = document.getElementById("signup")
 var signupModal = document.getElementById("signupModal")
 var signupClose = document.getElementById("signupClose")
+var alertClose = document.getElementById("alertClose")
+var alertButtonClose = document.getElementById("noThanks")
+var takeToSignUp = document.getElementById("takeToSignUp")
 
 signupButton.onclick = function() {
     signupModal.style.display = 'block';
@@ -48,3 +51,35 @@ signupButton.onclick = function() {
 signupClose.onclick = function() {
     signupModal.style.display = 'none';
 }
+
+
+
+const debounce = (func, delay) => { 
+    let debounceTimer 
+    return function() { 
+        const context = this
+        const args = arguments 
+            clearTimeout(debounceTimer) 
+                debounceTimer 
+            = setTimeout(() => func.apply(context, args), delay) 
+    } 
+}  
+
+window.addEventListener('scroll', debounce(function() { 
+   
+alertModal.style.display = 'block';
+     }, 5000)); 
+
+
+     alertClose.onclick = function() {
+        alertModal.style = 'none';
+    }
+
+    alertButtonClose.onclick = function() {
+        alertModal.style = 'none';
+    }
+    takeToSignUp.onclick = function () {
+        alertModal.style = 'none';
+        signupModal.style.display = 'block';
+    }
+
